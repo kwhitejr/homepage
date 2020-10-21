ce15a46b904312434df3fa763ac502d65873c806
Kevin White <kwhitejr@gmail.com>
2020-10-21 08:13:22 -0700

    Bugfix/terraform setup (#7)
    * install published
    
    * remove interpolations and bucket is publich
    
    * comment out alias
    
    * deprecate inline env vars
    
    * corrections to cloudfront and route 53 records

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -15,10 +15,3 @@ variable "website_bucket_name" {
   description = "the S3 bucket name"
   default     = "kwhitejr.com" # in this case is the same as the domain name
 }
-
-# Replace this with a data source
-# variable "website_zone_id" {
-#   type        = string
-#   description = "the Route 53 zone id"
-#   default     = "Z06300922KHV41EH3VE0R"
-# }
